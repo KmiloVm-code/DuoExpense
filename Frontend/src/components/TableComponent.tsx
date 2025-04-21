05c19aaba44a062fc9de5b3fb461976f1f4d9fa9
Andrés Camilo Vélez Mejía <kmilo7v@gmail.com>
2025-04-20 23:23:17 -0500

    update TableComponent to use DataTable and simplify TransactionPage

--- Frontend/src/components/TableComponent.tsx
+++ Frontend/src/components/TableComponent.tsx
@@ -1,142 +1,85 @@
-import { Key, useCallback, useState } from 'react'
-import { Button } from '@heroui/react'
-
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable
+} from '@tanstack/react-table'
 import {
   Table,
-  TableHeader,
   TableBody,
-  TableColumn,
-  TableRow,
-  TableCell
-} from '@heroui/table'
-import { Card, CardHeader, CardBody, CardFooter } from '@heroui/card'
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '../components/ui/table'
 
-type TableProps<T extends { concepto: string }> = {
-  toggleModal: (item: T) => void
-  data: T[]
-  dataDelete: (id: number) => void
-  columns: { key: keyof T | string; label: string }[]
-  formatDate?: (value: string) => string
-  formatValue?: (value: number) => string
-  getId: (item: T) => number
-  renderActions?: (item: T) => JSX.Element
-  renderItems?: (item: T, columnKey: Key) => JSX.Element
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
 }
 
-const TableComponent = <T extends { concepto: string }>({
-  toggleModal,
-  data,
-  dataDelete,
+function DataTable<TData, TValue>({
   columns,
-  formatDate = (value) => value,
-  formatValue = (value) => value.toString(),
-  getId,
-  renderItems,
-  renderActions
-}: TableProps<T>) => {
-  const [, setError] = useState<unknown | null>(null)
-
-  const handleEdit = useCallback(
-    (item: T) => () => {
-      toggleModal(item)
-    },
-    [toggleModal]
-  )
-
-  const handleDelete = (item: T) => {
-    const id = getId(item)
-    try {
-      dataDelete(id)
-    } catch (error) {
-      setError(error)
-      throw new Error('Error al eliminar el item')
-    }
-  }
-
-  const dataCell = (item: T, columnKey: Key) => {
-    const value = item[columnKey as keyof T]
-
-    switch (columnKey) {
-      case 'valor':
-        return <span>{formatValue(value as unknown as number)}</span>
-      case 'fecha':
-        return <span>{formatDate(value as unknown as string)}</span>
-      case 'actions':
-        return renderActions ? (
-          renderActions(item)
-        ) : (
-          <div className="flex gap-2 justify-center">
-            <Button color="secondary" variant="flat" onPress={handleEdit(item)}>
-              Editar
-            </Button>
-            <Button
-              color="danger"
-              variant="light"
-              onPress={() => handleDelete(item)}
-            >
-              Eliminar
-            </Button>
-          </div>
-        )
-      default:
-        return renderItems ? (
-          renderItems(item, columnKey)
-        ) : (
-          <span>{String(value)}</span>
-        )
-    }
-  }
+  data
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel()
+  })
 
   return (
-    <>
-      <Table
-        className="hidden md:block w-full"
-        aria-label="Data Table"
-        isHeaderSticky
-        isStriped
-        fullWidth
-      >
-        <TableHeader>
-          {columns.map((column) => (
-            <TableColumn key={column.key as string} align="center">
-              {column.label}
-            </TableColumn>
+    <div className="flex items-center justify-between overflow-hidden border border-solid border-purple-100 rounded-2xl mb-4">
+      <Table>
+        <TableHeader className="bg-purple-50">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead className="text-purple-600" key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
           ))}
         </TableHeader>
-        <TableBody<T> emptyContent={<p>No Hay Items</p>} items={data}>
-          {(item) => (
-            <TableRow key={getId(item)}>
-              {(columnKey) => (
-                <TableCell>{dataCell(item, columnKey)}</TableCell>
-              )}
+        <TableBody className="text-sm text-gray-700">
+          {table.getRowModel().rows.length > 0 ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && 'selected'}
+                className="hover:bg-purple-50"
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell
+                    style={{
+                      borderBottom: '1px solid #f3e8ff'
+                    }}
+                    key={cell.id}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="text-center">
+                No hay transacciones
+              </TableCell>
             </TableRow>
           )}
         </TableBody>
       </Table>
-
-      <div className="md:hidden w-full">
-        {data.map((item) => (
-          <Card key={getId(item)} className="mb-3">
-            <CardHeader>
-              <h1 className="text-lg font-bold">{item.concepto}</h1>
-            </CardHeader>
-            <CardBody className="flex flex-col gap-2">
-              {columns.map(
-                (column) =>
-                  column.key !== 'actions' && (
-                    <p key={column.key as string}>
-                      <span className="font-bold">{column.label}:</span>{' '}
-                      {dataCell(item, column.key as Key)}
-                    </p>
-                  )
-              )}
-            </CardBody>
-            <CardFooter>{dataCell(item, 'actions')}</CardFooter>
-          </Card>
-        ))}
-      </div>
-    </>
+    </div>
   )
 }
 
-export default TableComponent
+export default DataTable
--- Frontend/src/pages/TransactionPage.tsx
+++ Frontend/src/pages/TransactionPage.tsx
@@ -1,22 +1,11 @@
 import { useDataTable } from '../hooks/useDataTable'
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable
-} from '@tanstack/react-table'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow
-} from '../components/ui/table'
+
 import { Transaction } from '../types/Transaction'
 import { convertValue } from '../utils/formatters'
 import { CreditCard } from 'lucide-react'
 import { useDataContext } from '../contexts/DataContext'
+import { ColumnDef } from '@tanstack/react-table'
+import DataTable from '../components/TableComponent'
 
 const TransactionPage = () => {
   const { pickerValue } = useDataContext()
@@ -24,7 +13,6 @@ const TransactionPage = () => {
     `startDate=${pickerValue.start}&endDate=${pickerValue.end}`
   )
 
-  console.log('Transactions:', transactions)
   const columns: ColumnDef<Transaction>[] = [
     {
       accessorKey: 'transactionDate',
@@ -118,78 +106,6 @@ const TransactionPage = () => {
       }
     }
   ]
-
-  interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[]
-    data: TData[]
-  }
-
-  function DataTable<TData, TValue>({
-    columns,
-    data
-  }: DataTableProps<TData, TValue>) {
-    const table = useReactTable({
-      data,
-      columns,
-      getCoreRowModel: getCoreRowModel()
-    })
-
-    return (
-      <div className="flex items-center justify-between overflow-hidden border border-solid border-purple-100 rounded-2xl mb-4">
-        <Table>
-          <TableHeader className="bg-purple-50">
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead className="text-purple-600" key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody className="text-sm text-gray-700">
-            {table.getRowModel().rows.length > 0 ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && 'selected'}
-                  className="hover:bg-purple-50"
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell
-                      style={{
-                        borderBottom: '1px solid #f3e8ff'
-                      }}
-                      key={cell.id}
-                    >
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={columns.length} className="text-center">
-                  No hay transacciones
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-    )
-  }
 
   return (
     <div className="flex flex-col gap-4 p-4 bg-white rounded-2xl shadow-md">
